5a3049bc09ac4637addd9a8e07a1ee8d9f5a472b
pratul baheti <pratul baheti>
2016-08-22 11:21:12 -0400

    New Problems

--- src/leetcode/ReconstructItenary.java
+++ src/leetcode/ReconstructItenary.java
@@ -7,17 +7,20 @@ public class ReconstructItenary {
 	public static void main(String args[]) {
 		
 //		String[][] tickets = {{"JFK", "MUC"},{"MUC", "LHR"}, {"LHR", "SFO"}, {"SFO", "SJC"}};
-		String[][] tickets = {{"JFK","SFO"},{"JFK","ATL"},{"SFO","ATL"},{"ATL","JFK"},{"ATL","SFO"}};
+//		String[][] tickets = {{"JFK","ATL"},{"ATL","JFK"},{"JFK","SFO"},{"SFO","ATL"},{"ATL","SFO"}};
+		
+		String[][] tickets = {{"CBR","JFK"},{"TIA","EZE"},{"AUA","TIA"},{"JFK","EZE"},{"BNE","CBR"},{"JFK","CBR"},{"CBR","AUA"},{"EZE","HBA"},{"AXA","ANU"},{"BNE","EZE"},{"AXA","EZE"},{"AUA","ADL"},{"OOL","JFK"},{"BNE","AXA"},{"OOL","EZE"},{"EZE","ADL"},{"TIA","BNE"},{"EZE","TIA"},{"JFK","AUA"},{"AUA","EZE"},{"ANU","ADL"},{"TIA","BNE"},{"EZE","OOL"},{"ANU","BNE"},{"EZE","ANU"},{"ANU","AUA"},{"BNE","ANU"},{"CNS","JFK"},{"TIA","ADL"},{"ADL","AXA"},{"JFK","OOL"},{"AUA","ADL"},{"ADL","TIA"},{"ADL","ANU"},{"ADL","JFK"},{"BNE","EZE"},{"ANU","BNE"},{"JFK","BNE"},{"EZE","AUA"},{"EZE","AXA"},{"AUA","TIA"},{"ADL","CNS"},{"AXA","AUA"}};
 		String sb = new String();
-		for(String s: findItinerary(tickets)) {
-			sb=s+"-->"+sb;
+		System.out.print("JFK");
+		findItinerary(tickets);
+		for(;!list.isEmpty();) {
+			System.out.print("-->"+list.pop());
 		}
-		System.out.println(sb);
 	}
 	
-	static HashMap<String,TreeSet<String>> map = new HashMap<String,TreeSet<String>>();
-	static List<String> list = new LinkedList<String>();
-//	static LinkedHashMap<String,LinkedHashSet<String>> iter = new LinkedHashMap<String,LinkedHashSet<String>>();
+	static HashMap<String,PriorityQueue<String>> map = new HashMap<String,PriorityQueue<String>>();
+	static HashMap<String,LinkedList<String>> storage = new HashMap<String,LinkedList<String>>();
+	static Stack<String> list = new Stack<String>();
 	static int size =1;
 	static int total = 0;
 	
@@ -26,9 +29,9 @@ public class ReconstructItenary {
 			if (map.containsKey(s[0])) {
 				map.get(s[0]).add(s[1]);
 			} else {
-				TreeSet<String> set = new TreeSet<String>();
-				set.add(s[1]);
-				map.put(s[0], set);
+				PriorityQueue<String> pq = new PriorityQueue<String>();
+				pq.add(s[1]);
+				map.put(s[0], pq);
 			}
 		}
 		total=tickets.length;
@@ -38,26 +41,41 @@ public class ReconstructItenary {
     
     public static boolean recursion(String s) {
     	
+    	if(map.get(s).isEmpty() && storage.containsKey(s)) {
+    		map.get(s).addAll(storage.get(s));
+    		storage.remove(s);
+    	}
+    	if(s.equals("EZE") && map.get(s).peek().equals("HBA")) {
+    		System.out.println("debug");
+    	}
 		if(size == total) {
-			list.add(s);
+			list.push(map.get(s).peek());
 			return true;
 		}
-    	for(final Iterator<String> iterator = map.get(s).iterator(); iterator.hasNext();) {
-    		String to = iterator.next().toString();
+		PriorityQueue<String> pq = map.get(s);
+		while(!pq.isEmpty()) {
+    		String to = pq.remove();
 			if(map.containsKey(to)) {
-				iterator.remove();
     			++size;
     			boolean value = recursion(to);
     			if(value) {
-    				list.add(s);
+    				list.push(to);
+    				return true;
     			} else {
-    				map.get(s).add(to);
+    				pq.add(to);
     				--size;
     			}
     		} else {
-    			return false;
+    			if(storage.containsKey(s)) {
+    				storage.get(s).add(to);
+    			} else {
+    				LinkedList<String> dummy = new LinkedList<String>();
+    				dummy.add(to);
+    				storage.put(s, dummy);
+    			}
+    			//return false;
     		}
     	}
-    	return true;
+    	return false;
     }
 }
